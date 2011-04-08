5421951e6637e3becc45aa04244c2e2e66f77af1
Hannes Magnusson <bjori@redpill-linpro.com>
2011-04-08 23:05:08 +0200

    Remove the weird maxlifetime cleanup patch

--- debian/php5-common.php5.cron.d
+++
@@ -1,7 +0,0 @@
-# /etc/cron.d/php5: crontab fragment for php5
-#  This purges session files older than X, where X is defined in seconds
-#  as the largest value of session.gc_maxlifetime from all your php.ini
-#  files, or 24 minutes if not defined.  See /usr/lib/php5/maxlifetime
-
-# Look for and purge old sessions every 30 minutes
-09,39 *     * * *     root   [ -x /usr/lib/php5/maxlifetime ] && [ -d /var/lib/php5 ] && find /var/lib/php5/ -depth -mindepth 1 -maxdepth 1 -type f -cmin +$(/usr/lib/php5/maxlifetime) -delete
